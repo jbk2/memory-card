87964c519f526c5ab95f837d38984bdc6dc77b0e
jbk2 <james@humanbenefits.com>
2025-04-14 12:12:32 +0200

    Scaffold App fetching from dog API, & update to latest react & react-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import GameBoard from './components/GameBoard.jsx'
 import Header from './components/Header.jsx'
 import './App.css'
 
 function App() {
   const [ score, setScore ] = useState(0)
+  const [ images, setImages ] = useState([])
+
+  useEffect(() => {
+    console.log('Here are your images: ==>>', images);
+  }, [images]);
+
+  useEffect(() => {
+    async function getImages() {
+      try {
+        const url = 'https://dog.ceo/api/breed/dachshund/images/random/8'
+        const response = await fetch(url)
+        
+        if (response.ok) {
+          const data = await response.json()
+          setImages(data.message); 
+        } else {
+          throw Error('response failed');
+        }
+      } catch(error) {
+        console.error(error)
+      }
+    }
+
+    getImages();
+  }, []);
 
   return (
     <>
       <Header score={score} />
-      <GameBoard />
+      <GameBoard images={images} />
     </>
   )
 }
